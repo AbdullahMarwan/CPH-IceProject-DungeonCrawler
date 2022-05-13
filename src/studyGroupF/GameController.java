586f378fc7e106dc7d7a6a88edfaaca14805ecac
Emilio Castro Lagunas <EmilioCLDK@gmail.com>
2022-05-13 12:17:07 +0200

    Filled gameController class

--- src/studyGroupF/GameController.java
+++ src/studyGroupF/GameController.java
@@ -1,4 +1,16 @@
 package studyGroupF;
 
+import java.util.ArrayList;
+
 public class GameController {
+
+    ArrayList<Level> levels;
+
+    public void initializePlayer(){
+
+    }
+
+    private void saveData(){
+        
+    }
 }
