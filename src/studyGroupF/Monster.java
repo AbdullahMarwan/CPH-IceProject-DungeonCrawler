d8d4ab74e56758c12af6c757f9b5bc9f7ce432aa
Emilio Castro Lagunas <EmilioCLDK@gmail.com>
2022-05-16 10:30:24 +0200

    Started on MonsterData & Class

--- src/studyGroupF/Monster.java
+++ src/studyGroupF/Monster.java
@@ -9,11 +9,25 @@ public class Monster {
 
     Level level = new Level();
 
+    public Monster(String monsterName, int HP, int damage) {
+        this.monsterName = monsterName;
+        this.HP = HP;
+        this.damage = damage;
+
+    }
+
     public void createMonster(){
         String[] monsterName = {"Skeleton","Zombie"};
-        HP = (10*level.currentLevel());
+        int skeletonBaseHP = 5;
+        int skeletonHP = (skeletonBaseHP * level.currentLevel());
         damage = (1*level.currentLevel());
+    }
 
+    public Monster newSkeleton(){
+        return new Monster("Skeleton", createMonster().skeletonHP, 5);
+    }
+    public static Monster newZombie(){
+        return new Monster("Zombie", newZombie().HP, 10);
     }
 
 }
